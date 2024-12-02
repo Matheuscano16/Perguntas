03a82cb13a12c7cc5b90921267459c01a75a4f72
Matheuscano16 <matheuscano25@gmail.com>
2024-12-02 19:41:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é o Python e quais são suas principais características?',
+    'O Python é uma linguagem de programação de alto nível, interpretada e de propósito geral.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo em termos de área?',
+    'O maior continente do mundo em termos de área é a Ásia, com uma área de aproximadamente 44,58 milhões de quilômetros quadrados, o que representa cerca de 30% da área total da Terra.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma variável em programação?',
+    'Uma variável é um espaço de memória usado para armazenar dados que podem ser alterados durante a execução de um programa. Em muitas linguagens de programação, uma variável é identificada por um nome, e os dados armazenados podem ser de diferentes tipos (inteiros, strings, listas, entre outros). O valor de uma variável pode ser modificado ou acessado conforme o programa avança.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual é a diferença entre as palavras "since" e "for" quando usadas em frases em inglês?',
+    'A diferença entre since e for no inglês está relacionada ao tempo. Since é usado para indicar o ponto de início de uma ação ou evento no passado, como uma data, hora ou momento específico. Por exemplo, eu vivo aqui desde 2010. Já for é usado para indicar a duração de uma ação ou evento, como um período de tempo. Por exemplo, eu vivo aqui há 10 anos. Portanto, since aponta para o momento em que algo começou, enquanto for indica quanto tempo algo durou.'
+)
